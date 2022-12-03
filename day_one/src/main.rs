e5256e23981825196d750a44754ee972302253e0
Fortunato De Angelis <deangelis.fortunato@gmail.com>
2022-12-03 20:00:40 +0100

    Day_01, Part 2

--- day_one/src/main.rs
+++ day_one/src/main.rs
@@ -5,8 +5,6 @@ use std::io::{BufReader, BufRead, Error};
 fn main() -> Result<(), Error> {
 
     let mut elf: Vec<i32> = Vec::new();
-
-
     // Get the file
     let path = "data/input.txt";
 
@@ -33,16 +31,38 @@ fn main() -> Result<(), Error> {
         }else {
             index += 1;
         }
-       
     }
 
     // Check the max value in vect
-    let max = elf.iter().max().unwrap();
+    let mut max = elf.iter().max().unwrap();
     
     // Check the index of max value
-    let index_max = elf.iter().position(|&r| r == *max).unwrap();
+    let mut index_max = elf.iter().position(|&r| r == *max).unwrap();
 
     println!("L'elfo {} ha totalizzato il maggior numero di calorie: {}", index_max, max);
+
+    // Sum all calories
+    let mut total_calories: i32= *max;
+
+    // Remove max value
+    elf.remove(index_max);
+
+    // Remove index with max value in element for three times
+    for _ in 0..2 {
+        // Check the max value in vect
+        max = elf.iter().max().unwrap();
+        
+        // Check the index of max value
+        index_max = elf.iter().position(|&r| r == *max).unwrap();
+
+        // add max value to total calories
+        total_calories += *max;
+
+        // Remove max value
+        elf.remove(index_max);
+    }
+
+    println!("Gli ultimi elfi hano totalizzato {} calorie", total_calories);
 
     Ok(())
 }
