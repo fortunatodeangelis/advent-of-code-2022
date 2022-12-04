1b3069001b7ce9da49a1efb4fe3b5832bf218690
Fortunato De Angelis <deangelis.fortunato@gmail.com>
2022-12-04 15:08:43 +0100

    Day 02, Part 1

---
+++ day_02/src/main.rs
@@ -0,0 +1,75 @@
+use std::fs::File;
+use std::io::{BufReader, BufRead, Error};
+
+fn main() -> Result<(), Error> {
+
+    // Get the file
+    let path = "data/input.txt";
+
+    // Open the file
+    let input = File::open(path)?;
+
+    // Create a buffered reader
+    let buffered = BufReader::new(input);
+
+    let mut elfs_rounds: Vec<i32> = Vec::new();
+    let mut your_rounds: Vec<i32> = Vec::new();
+
+    // Iterate over the lines
+    for line in buffered.lines() {
+        let line = line?;
+        let round: Vec<&str> = line.split(" ").collect();
+        let mut elfs_score = 0;
+        let mut your_score = 0;
+
+        // Convert points
+        elfs_score += convert_points(round[0]);
+        your_score += convert_points(round[1]);
+
+        let result = score_round(elfs_score, your_score);
+
+        // Check round result
+        if result == "1" {
+            elfs_score +=6;
+        }else if result == "2" {
+            your_score +=6;
+        } else{
+            elfs_score +=3;
+            your_score +=3;
+        }
+        your_rounds.push(your_score);
+        elfs_rounds.push(elfs_score);
+    }
+
+    // Sum all scores of round
+    let elfs_total_score: i32 = elfs_rounds.iter().sum();
+    let yours_total_score: i32 = your_rounds.iter().sum();
+
+    println!("Secondo la guida strategica hai totalizzato {} punti mentre l'elfo ha totalizzato {}", yours_total_score, elfs_total_score);
+    Ok(())
+}
+
+
+fn score_round(elfs_score:i32, your_score:i32) -> &'static str {
+    let r = if (elfs_score) % 3 == (your_score - 1) {
+        "2" // you win
+    } else if elfs_score == your_score {
+        "x" // draw
+    } else {
+        "1" // elf win
+    };
+    r
+}
+
+fn convert_points(char: &str) -> i32 {
+    let r = match char {
+        "A" => 1,
+        "B" => 2,
+        "C" => 3,
+        "X" => 1,
+        "Y" => 2,
+        "Z" => 3,
+        _ => 0
+    };
+    return r;
+}
